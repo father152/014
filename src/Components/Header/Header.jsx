641cb350765befe2f9a60f08e8bfbbd273e86959
Your Name <tsimbalenko.vv@gmail.com>
2023-11-29 16:29:14 +0200

    1

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <div className="header">
+      <ul className="header-list">
+        <li className="list">About As</li>
+        <li className="list">Products</li>
+        <li className="list">Portfolio</li>
+        <li className="list">Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export { Header };
